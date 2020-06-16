c545c6a355674de5227fe7ef79259acceea3476b
Florian Zaruba <zarubaf@iis.ee.ethz.ch>
2020-06-16 10:12:48 +0200

    axi: Update AXI dependency

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -13,6 +13,9 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 - AXI to reg interface
 - Opensource release
 
+### Changed
+- Updated AXI dependency
+
 ## 0.1.3 - 2018-06-02
 ### Fixed
 - Add `axi_lite_to_reg.sv` to list of source files.
--- src/axi_to_reg.sv
+++ src/axi_to_reg.sv
@@ -1,4 +1,4 @@
-// Copyright 2018 ETH Zurich and University of Bologna.
+// Copyright 2018-2020 ETH Zurich and University of Bologna.
 // Copyright and related rights are licensed under the Solderpad Hardware
 // License, Version 0.51 (the "License"); you may not use this file except in
 // compliance with the License.  You may obtain a copy of the License at
@@ -16,34 +16,43 @@ module axi_to_reg #(
   parameter int ADDR_WIDTH = -1,
   /// The width of the data.
   parameter int DATA_WIDTH = -1,
+  /// The width of the id.
+  parameter int ID_WIDTH = -1,
+  /// The width of the user signal.
+  parameter int USER_WIDTH = -1,
   /// Whether the AXI-Lite W channel should be decoupled with a register. This
   /// can help break long paths at the expense of registers.
   parameter bit DECOUPLE_W = 1
 )(
-  input  logic clk_i     ,
-  input  logic rst_ni    ,
-  input  logic testmode_i,
-  AXI_BUS.in   in        ,
-  REG_BUS.out  reg_o
+  input  logic  clk_i     ,
+  input  logic  rst_ni    ,
+  input  logic  testmode_i,
+  AXI_BUS.Slave in        ,
+  REG_BUS.out   reg_o
 );
 
-  AXI_LITE_BUS #(
+  AXI_LITE #(
     .AXI_ADDR_WIDTH ( ADDR_WIDTH ),
     .AXI_DATA_WIDTH ( DATA_WIDTH )
-  ) axi_lite (clk_i);
+  ) axi_lite ();
 
   //  convert axi to axi-lite
-  axi_to_axi_lite #(
-    /// Maximum number of outstanding reads.
-    .NUM_PENDING_RD (1),
+  axi_to_axi_lite_intf #(
+    .AXI_ADDR_WIDTH     ( ADDR_WIDTH ),
+    .AXI_DATA_WIDTH     ( DATA_WIDTH ),
+    .AXI_ID_WIDTH       ( ID_WIDTH   ),
+    .AXI_USER_WIDTH     ( USER_WIDTH ),
     /// Maximum number of outstanding writes.
-    .NUM_PENDING_WR (1)
+    .AXI_MAX_WRITE_TXNS ( 2 ),
+    /// Maximum number of outstanding reads.
+    .AXI_MAX_READ_TXNS  ( 2 ),
+    .FALL_THROUGH       ( 0 )
   ) i_axi_to_axi_lite (
     .clk_i,
     .rst_ni,
     .testmode_i,
-    .in,
-    .out ( axi_lite )
+    .slv ( in ),
+    .mst ( axi_lite )
   );
 
   axi_lite_to_reg #(
